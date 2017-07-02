17ac2406c86f659dfe2144f4c8fc273d45800480
王禹衡 <yuheng.wang@froginfo.cn>
2017-07-02 22:42:39 +0800

    add rules

--- src/vd-validate.js
+++ src/vd-validate.js
@@ -1,36 +1,83 @@
-const _rules = (function() {
+const _rules = errors => {
   const required = value => {
-    if(Array.isArray(value)) {
-      return value.length;
+    let error = false;
+    if(_isEmpty(value)) {
+      error = errors.required;
     }
-    return value;
+    return error;
   };
 
-  const number = value => !isNaN(value);
+  const number = value => {
+    let error = false;
+    if(isNaN(value)) {
+      error = errors.number;
+    }
+    return error;
+  };
 
   const phone = value => {
+    let error = false;
     const regex = /^1[34578]\d{9}$/;
-    return regex.test(value);
+    if(!regex.test(value)) {
+      error = errors.phone;
+    }
+    return error;
   };
 
   const identity = value => {
+    let error = false;
     const regex = /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$)/;
-    return regex.test(value);
+    if(!regex.test(value)) {
+      error = errors.identity;
+    }
+    return error;
+  };
+
+  const email = value => {
+    let error = false;
+    const regex = /\w+@\w+\.\w+/;
+    if(!regex.test(value)) {
+      error = errors.email;
+    }
+    return error;
+  };
+
+  const length = (value, param) => {
+    let error = false;
+    const l = value.length;
+    const min = param.split('-')[0];
+    const max = param.split('-')[1];
+
+    if(min > max) throw new ValidateError('min is bigger than max in rule "length"');
+    
+    if(min && !max && l < min) {
+      error = `长度不小于${min}个字符`;
+    }
+    if(max && !min && l > max) {
+      error = `长度不大于${max}个字符`;
+    }
+    if(min && max && (l < min || l > max)) {
+      error = `长度在${min}到${max}个字符`;
+    }
+    return error;
   };
 
   return {
     required,
     number,
     phone,
-    identity
+    identity,
+    email,
+    length
   };
-})();
+};
 
 const _errors = {
-  required: '请填写该项.',
-  number: '请填写数字.',
-  phone: '请填写手机号.',
-  identity: '请填写身份证号.',
+  required: '请填写该项',
+  number: '请填写数字',
+  phone: '请填写手机号',
+  identity: '请填写身份证号',
+  email: '请填写Email地址',
 };
 
 class ValidateError {
@@ -46,12 +93,8 @@ class ValidateError {
 class Validator {
   constructor(config) {
     this.errors = Object.assign({}, _errors, config.errors);
-    this.rules = Object.assign({}, _rules, config.rules);
+    this.rules = Object.assign({}, _rules(this.errors), config.rules);
     this.fields = {};
-  }
-
-  errors(obj) {
-    this.errors = Object.assign({}, this.errors, obj);
   }
 
   rules(obj) {
@@ -77,54 +120,74 @@ class Validator {
       msg,
       value,
       checks,
-      isReq: ~'required'.indexOf(checks)
+      isReq: ~Object.keys(checks).indexOf('required')
     };
   }
 
   validateField(name, value) {
     const field = this.fields[name];
-    const promises = field.checks.split('|').map(check => this.test(name, value, check));
 
-    return Promise.all(promises).then(results => {
-      const fails = results.filter(r => r !== true);
-
-      if(field.msg) {
-        if(fails.length) {
-          this.displayError(field.msg, fails[0].rule);
-        }
-        else this.hideError(field.msg);
-      }
+    if(!field.isReq && _isEmpty(value)) {
+      this.hideError(field.msg);
 
       return {
-        pass: !fails.length,
+        pass: true,
         name,
-        rules: fails.map(f => f.rule)
       };
-    });
-  }
-
-  test(name, value, check) {
-    const rule = this.rules[check];
-    const result = rule(value);
-
-    if(result) {
-      return Promise.resolve(true);
     }
-    else return Promise.resolve({ name, rule: check });
+
+    try {
+      const promises = Object.keys(field.checks).map(check => this.test(check, name, value, field.checks[check]));
+
+      return Promise.all(promises).then(results => {
+        const fails = results.filter(r => r !== true);
+
+        if(field.msg) {
+          if(fails.length) {
+            this.displayError(field.msg, fails[0].error);
+          }
+          else this.hideError(field.msg);
+        }
+
+        return {
+          pass: !fails.length,
+          name,
+          rules: fails.map(f => f.rule)
+        };
+      });
+    }
+    catch(err) {
+      console.error(err.msg);
+    }
   }
 
-  displayError(msg, rule) {
-    const error = this.getErrorMessage(rule);
+  test(check, name, value, param) {
+    const rule = this.rules[check];
+
+    try {
+      const error = rule(value, param);
+
+      if(!error) {
+        return Promise.resolve(true);
+      }
+      else return Promise.resolve({ name, rule: check, error });
+    } 
+    catch(err) {
+      if(err.constructor.name !== 'ValidateError') {
+        throw new ValidateError(`rule '${check}' is not properly defined`);        
+      }
+
+      throw err;
+    }
+  }
+
+  displayError(msg, error) {
     msg.innerText = error;
     msg.style.display = 'block';
   }
 
   hideError(msg) {
     msg.style.display = 'none';
-  }
-
-  getErrorMessage(rule) {
-    return this.errors[rule] || '请检查内容格式.';
   }
 }
 
@@ -135,11 +198,17 @@ class ValidateListener {
     this.vm = vnode.context;
     this.component = vnode.child;
     this.binding = binding;
-    this.name = this.getFieldName();
+    this.name = null;
+    try {
+      this.getFieldName();
+    }
+    catch(err) {
+      console.error(err.msg);
+    }
   }
 
   attach() {
-    let checks = this.binding.value;
+    let checks = this.getFieldChecks();
     this.createErrorMsg();
     this.vm.$validator.fill(this.name, checks, this.getFieldValue(), this.msg);
     this.attachFieldListener();
@@ -151,7 +220,7 @@ class ValidateListener {
     const nativeListener = _wait(() => this.vm.$validator.validateField(this.name, this.el.value), this.getFieldDelay());
 
     if(this.component) {
-      this.component.$on(`${this.config.emit || 'input'}`, componentListener);
+      this.component.$on(this.config.emit, componentListener);
     }
     else {
       switch(this.el.tagName) {
@@ -168,6 +237,17 @@ class ValidateListener {
     }
   }
 
+  getFieldChecks() {
+    const str = this.binding.value;
+    const checks = {};
+    str.split('|').forEach(item => {
+      const check = item.split(':');
+      checks[check[0]] = check[1] || null;
+    });
+
+    return checks;
+  }
+
   getFieldValue() {
     if(this.component) {
       return () => this.component.value;
@@ -181,7 +261,7 @@ class ValidateListener {
     if(!name) {
       throw new ValidateError('value of "data-vd-name" must be given');
     }
-    return name;
+    this.name = name;
   }
 
   getFieldDelay() {
@@ -259,8 +339,17 @@ const _wait = (fn, delay = 0) => {
   };
 };
 
+const _isEmpty = value => {
+  if(Array.isArray(value)) {
+    return !value.length;
+  }
+
+  return !value;
+};
+
 const _defaultOptions = {
-  showErrorMsg: true
+  showErrorMsg: true,
+  emit: 'input'
 };
 
 const _makeMixin = config => ({
